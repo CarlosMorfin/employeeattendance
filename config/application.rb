f8a1dc6b9bd2fe98927ca8cefe6ca651f0457057
Carlos Morfin Diaz <carlosmorfindiaz@gmail.com>
2019-08-22 00:00:21 -0500

    Define traslations directory and set spanish as main language

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,19 @@ module Employeeattendance
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Internationalization and localization config
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :'es-MX'
+
+    config.time_zone = 'Mexico City'
+    config.active_record.time_zone_aware_types = [:datetime, :time]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.active_record.belongs_to_required_by_default = false
+    config.active_record.pluralize_table_names = false
   end
 end
