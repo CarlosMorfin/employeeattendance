adfb3fbb953e69d59d89e5bd9d70658f3e09c5bf
Carlos Morfin Diaz <carlosmorfindiaz@gmail.com>
2019-08-24 17:45:35 -0500

    Add navigation bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  helper_method :active_main_navbar
+  helper_method :active_nav_tab
+
   private
+
+  def active_main_navbar ; end
+  def active_nav_tab ; end
 
   def after_sign_in_path_for(resource)
     admin_root_path
